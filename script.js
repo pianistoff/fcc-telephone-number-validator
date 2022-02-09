9174732102174f74d7daf7e85d10114329737a00
Severyn Hundiak <severyn.h@gmail.com>
2022-02-09 03:44:03 +0200

    Accept additional white spaces at beginning and end

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function telephoneCheck(str) {
-  return /^\+?1?[-.\s]?(\(\d{3}\)|\d{3})[-.\s]?\d{3}[-.\s]?\d{4}$/.test(str);
+  return /^\s*\+?1?[-.\s]?(\(\d{3}\)|\d{3})[-.\s]?\d{3}[-.\s]?\d{4}\s*$/.test(str);
 }
 
 const title = document.getElementById("title");
